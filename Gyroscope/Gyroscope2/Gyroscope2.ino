71c4d186cc389ae59ea0bc8df21b0b2d4b2a062b
davidgodinez <davidgodinez70@gmail.com>
2022-10-14 11:14:42 -0500

    Improve arduino script to officially send first set of gyroscope values. Improve Python script to receive and read gyroscope values :)

---
+++ Gyroscope/Gyroscope2/Gyroscope2.ino
@@ -0,0 +1,106 @@
+// Here does nothing 
+
+/*
+  Arduino LSM9DS1 - Simple Accelerometer
+
+  This example reads the acceleration values from the LSM9DS1
+  sensor and continuously prints them to the Serial Monitor
+  or Serial Plotter.
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense
+
+  created 10 Jul 2019
+  by Riccardo Rizzo
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+#include <Arduino_LSM9DS1.h>
+
+BLEService customService("17c73c1a-4bc7-11ed-bdc3-0242ac120002");
+
+// BLEIntCharacteristic CharacteristicX("17c73c1a-4bc7-11ed-bdc3-0242ac120002", BLERead | BLENotify);
+BLEIntCharacteristic CharacteristicY("17c73c1a-4bc7-11ed-bdc3-0242ac120002", BLERead | BLENotify);
+// BLEIntCharacteristic CharacteristicZ("17c73c1a-4bc7-11ed-bdc3-0242ac120002", BLERead | BLENotify);
+
+int plusThreshold = 30, minusThreshold = -30;
+
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Gyroscope in degrees/second");
+
+  // begin initialization
+  BLE.begin();
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("BLE");
+  BLE.setAdvertisedService(customService);
+
+  // add the characteristic to the service
+  // customService.addCharacteristic(CharacteristicX);
+  customService.addCharacteristic(CharacteristicY);
+  // customService.addCharacteristic(CharacteristicZ);
+
+  // add service
+  BLE.addService(customService);
+
+  // set the initial value for the characeristic:
+  // CharacteristicX.writeValue(0);
+  CharacteristicY.writeValue(0);
+  // CharacteristicZ.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE LED Peripheral");
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  
+}
+
+void loop() {
+   BLEDevice central = BLE.central();
+   if (central) {
+     Serial.print("Connected to central ");
+     Serial.print(central.address());
+     digitalWrite(LED_BUILTIN, HIGH);
+   }
+  // BLE.poll();
+   while (central.connected()) {
+
+    float x, y, z;
+
+    if (IMU.gyroscopeAvailable()) {
+      IMU.readGyroscope(x, y, z);
+    }
+    if(y > plusThreshold)
+    {
+      Serial.println("Up");
+      delay(500);
+    }
+    if(y < minusThreshold)
+    {
+      Serial.println("Down");
+      delay(500);
+    }  
+    // CharacteristicX.writeValue(x*100);
+    CharacteristicY.writeValue(y*100);
+    // CharacteristicZ.writeValue(z*100);
+  }
+}
+
---
+++ ProtoStax_Arduino_Nano_BLE_RGBLED_Service/rgbBLE_Central2.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+/******************************************************************* 
+  ProtoStax Arduino Nano 33 BLE Sense RGB LED Control Central Device
+
+  This is a example sketch for controlling the RGB LED on an 
+  Arduino Nano 33 BLE Sense with Bluetooth over Python  
+   
+  Items used:
+  Arduino Nano 33 BLE Sense
+  ProtoStax for BreadBoard/Custom Boards - 
+      - to house and protect the Nano and allow for other circuitry 
+      --> https://www.protostax.com/collections/all/products/protostax-for-breadboard
+  
+  The Nano publishes a Bluetooth LE Client profile with Characteristics for the Red, Green, 
+  and Blue components of the onboard RGB LED. These can be read and written to
+  control the LED colors.
+
+  This program toggles the R,G,B LEDs based on user input. Run the python program from your computer
+  (PC, Mac or Linux) that has Bluetooth support and the requisite python packages - 
+  you can then read and set the on/off states of the 3 colors. 
+  
+  The Red, Green and Blue colors of the onboard RGB LED can only be turned on or off. 
+  It is not possible to use PWM to mix colors, unfortunately, based on how the Arduino 
+  Nano BLE Sense board is configured.
+  
+  We write a value of 1 to turn on a color and 0 to turn it off. The user inputs 
+  a string that can contain r,g,b (or any combination) and those colors will be toggled. 
+
+  The Arduino Nano 33 BLE Sense is chockful of other sensors - you can similarly expose 
+  those sensors data as Characteristics
+ 
+  Written by Sridhar Rajagopal for ProtoStax
+  BSD license. All text above must be included in any redistribution
+ */
+"""
+
+
+from codecs import utf_8_decode
+import logging
+import asyncio
+import platform
+import ast
+
+from bleak import BleakClient, BleakScanner, discover
+
+# These values have been randomly generated - they must match between the Central and Peripheral devices
+# Any changes you make here must be suitably made in the Arduino program as well
+
+# RED_LED_UUID = '13012F01-F8C3-4F4A-A8F4-15CD926DA146'
+# GREEN_LED_UUID = '13012F02-F8C3-4F4A-A8F4-15CD926DA146'
+# BLUE_LED_UUID = '13012F03-F8C3-4F4A-A8F4-15CD926DA146'
+
+# X_AXIS_UUID = "17c73c1a-4bc7-11ed-bdc3-0242ac120002"
+Y_AXIS_UUID = "17c73c1a-4bc7-11ed-bdc3-0242ac120002"
+# Z_AXIS_UUID = "17c73c1a-4bc7-11ed-bdc3-0242ac120002"
+
+
+on_value = bytearray([0x01])
+off_value = bytearray([0x00])
+
+
+
+def getValue(on):
+    if on:
+        return on_value
+    else:
+        return off_value
+
+    
+
+async def run():
+    global RED, GREEN, BLUE
+
+    print('BLE LED Peripheral Central Service')
+    print('Looking for BLE Sense Peripheral Device...')
+
+    found = False
+    devices = await discover()
+    for d in devices:       
+        if 'BLE'in d.name:
+            print('Found BLE Peripheral')
+            found = True
+            async with BleakClient(d.address) as client:
+                print("Services")
+                for service in client.services:
+                    print(service)
+                print(f'Connected to {d.address}')
+                val = bytes(await client.read_gatt_char(Y_AXIS_UUID))
+                # new_val = val.decode()
+                val = str(val)
+                for i in range(len(val)):
+                    print(ord(val[i]))
+
+    if not found:
+        print('Could not find Arduino Nano 33 BLE Sense Peripheral')
+
+                    
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(run())
+except KeyboardInterrupt:
+    print('\nReceived Keyboard Interrupt')
+finally:
+    print('Program finished')
+        
