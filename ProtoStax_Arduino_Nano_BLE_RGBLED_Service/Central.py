2c07f90f33566c59020599da507f8b8249f89669
davidgodinez <davidgodinez70@gmail.com>
2022-10-20 00:00:17 -0500

    Add requirements.txt

--- ProtoStax_Arduino_Nano_BLE_RGBLED_Service/Central.py
+++ ProtoStax_Arduino_Nano_BLE_RGBLED_Service/Central.py
@@ -19,20 +19,17 @@
   their workout. 
 """
 
-from codecs import utf_8_decode
-import logging
 import asyncio
-import platform
-import ast
 from time import sleep
 from azure.iot.device import IoTHubDeviceClient
 from azure.iot.device import Message
-from bleak import BleakClient, BleakScanner, discover
+from bleak import BleakClient, BleakScanner
 
 # These values have been randomly generated - they must match between the Central and Peripheral devices
 # Any changes you make here must be suitably made in the Arduino program as well
-
 Y_AXIS_UUID = "17c73c1a-4bc7-11ed-bdc3-0242ac120002"
+
+# Connection string taken from iothub device. 
 CONNECTION_STRING = "HostName=davidiothub2.azure-devices.net;DeviceId=raspberrypi;SharedAccessKey=yxztDdIOVP3vWCfpyF3G7N1KljKhWFEEkWuYrlyl0Kg="
 
 
@@ -46,9 +43,10 @@ def iothub_client_init():
 async def run():
     new_messenger = iothub_client_init()
     print('BLE Peripheral Central Service')
-    print('Looking for BLE Sense Peripheral Device...')
     print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+    print('Looking for BLE Sense Peripheral Device...')
 
+    # Find device
     found = False
     devices = await BleakScanner.discover()
     for d in devices:       
@@ -61,7 +59,10 @@ async def run():
                     print(service)
                 print(f'Connected to {d.address}')
                 counter = 0
+
+                # Main loop. Listening for arduino messages and loggin in counter variable. 
                 while True:
+
                     val = bytes(await client.read_gatt_char(Y_AXIS_UUID))
                     # print(val)
                     val1 = str(val)
@@ -81,20 +82,21 @@ async def run():
                         counter_message = Message(msg_txt_formatted)
                         print(counter_message)
                         new_messenger.send_message(counter_message)
-                        print('message successfully sent! :`)')
+                        print('message successfully sent!')
                     sleep(0.5)
 
     if not found:
         print('Could not find Arduino Nano 33 BLE Sense Peripheral')
 
-# define as main():                     
-loop = asyncio.get_event_loop()
-try:
-    loop.run_until_complete(run())
-except KeyboardInterrupt:
-    print('\nReceived Keyboard Interrupt')
-finally:
-    print('Program finished')
+def main():                     
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(run())
+    except KeyboardInterrupt:
+        print('\nReceived Keyboard Interrupt')
+    finally:
+        print('Program finished')
 
 
-        
+if __name__ == '__main__':
+    main()
