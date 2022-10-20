dcda1b1fcc9c43cdfce34e4c5f6e5df56d16c478
davidgodinez <davidgodinez70@gmail.com>
2022-10-19 23:25:06 -0500

    Add rgbBLE_Central3 which has streaming functionality to iothub

--- ProtoStax_Arduino_Nano_BLE_RGBLED_Service/rgbBLE_Central3.py
+++ ProtoStax_Arduino_Nano_BLE_RGBLED_Service/rgbBLE_Central3.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-/******************************************************************* 
-
   Gyroscope Arduino Nano Nano BLE Control Central Device
 
   This is the main file for receiving data from Arduino Nano BLE. 
@@ -19,22 +17,7 @@
   This program counts the number of reps a person does at a specific machine at the gym and streams the data to Azure IoT.
   From there the data is streamed back to an app on the user's phone where they can have a permanent and accurate log of 
   their workout. 
-  
-  The Red, Green and Blue colors of the onboard RGB LED can only be turned on or off. 
-  It is not possible to use PWM to mix colors, unfortunately, based on how the Arduino 
-  Nano BLE Sense board is configured.
-  
-  We write a value of 1 to turn on a color and 0 to turn it off. The user inputs 
-  a string that can contain r,g,b (or any combination) and those colors will be toggled. 
-
-  The Arduino Nano 33 BLE Sense is chockful of other sensors - you can similarly expose 
-  those sensors data as Characteristics
- 
-  Written by Sridhar Rajagopal for ProtoStax
-  BSD license. All text above must be included in any redistribution
- */
 """
-
 
 from codecs import utf_8_decode
 import logging
@@ -42,19 +25,30 @@ import asyncio
 import platform
 import ast
 from time import sleep
+from azure.iot.device import IoTHubDeviceClient
+from azure.iot.device import Message
 from bleak import BleakClient, BleakScanner, discover
 
 # These values have been randomly generated - they must match between the Central and Peripheral devices
 # Any changes you make here must be suitably made in the Arduino program as well
 
-# X_AXIS_UUID = "17c73c1a-4bc7-11ed-bdc3-0242ac120002"
 Y_AXIS_UUID = "17c73c1a-4bc7-11ed-bdc3-0242ac120002"
-# Z_AXIS_UUID = "17c73c1a-4bc7-11ed-bdc3-0242ac120002"
+
+CONNECTION_STRING = "HostName=davidiothub2.azure-devices.net;DeviceId=raspberrypi;SharedAccessKey=yxztDdIOVP3vWCfpyF3G7N1KljKhWFEEkWuYrlyl0Kg="
+
+
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    new_messenger = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return new_messenger
 
 
 async def run():
+    new_messenger = iothub_client_init()
     print('BLE Peripheral Central Service')
     print('Looking for BLE Sense Peripheral Device...')
+    print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
 
     found = False
     devices = await discover()
@@ -76,16 +70,24 @@ async def run():
                         print('None')
                     #TODO include logic that defaulst back to 0 after a value has been counted. 
                     # I.e if still 'up' it won't continue counting but go back to 0
-                    
-                    # elif val1 == str(b'\x00\x00\x00\x00'):
-                    #     print('None')
                     else:
-                        # print(bytes(val1,encoding='utf-8'))
-                        
-                        # for i in range(len(val)):
-                        #     print(ord(val1[i]))
                         counter= counter + 1
                         print(counter)
+                        print(f'sending message {counter}')
+                #         # Build the message with simulated telemetry values.
+                #         first = From
+                #         second = To
+                #         msg_txt_formatted = MSG_TXT.format(first=first, second=second)
+                #         message = Message(msg_txt_formatted)
+                        first = 'Raspberry Pi'
+                        second = str(counter)
+                        MSG_TXT = '{{"From": {first},"Message": {second}}}'
+
+                        msg_txt_formatted = MSG_TXT.format(first=first, second=second)
+                        counter_message = Message(msg_txt_formatted)
+                        print(counter_message)
+                        new_messenger.send_message(counter_message)
+                        print('message successfully sent! :`)')
                     sleep(0.5)
 
     if not found:
@@ -94,9 +96,41 @@ async def run():
                     
 loop = asyncio.get_event_loop()
 try:
+    client = iothub_client_init()
+    print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
     loop.run_until_complete(run())
 except KeyboardInterrupt:
     print('\nReceived Keyboard Interrupt')
 finally:
     print('Program finished')
+
+
+# # Define the JSON message to send to IoT Hub.
+# From = "Pi"
+# To = "Azure"
+# MSG_TXT = '{{"From": {first},"To": {second}}}'
+
+
+
+# def iothub_client_telemetry_sample_run():
+
+    # try:
+    #     client = iothub_client_init()
+    #     print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+    #     while True:
+    #         # Build the message with simulated telemetry values.
+    #         first = From
+    #         second = To
+    #         msg_txt_formatted = MSG_TXT.format(first=first, second=second)
+    #         message = Message(msg_txt_formatted)
+
+    #         # Send the message.
+    #         print( "Sending message: {}".format(message) )
+    #         client.send_message(message)
+    #         print ( "Message successfully sent" )
+    #         sleep(3)
+
+    # except KeyboardInterrupt:
+    #     print ( "IoTHubClient sample stopped" )
+
         
